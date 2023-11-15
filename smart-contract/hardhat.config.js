cfb26d4c1b5149bf190d886d82ef8be67f29fb2e
Joseph Ebin <josefophe@gmail.com>
2023-11-15 22:21:03 +0100

    Update hardhat.config.js

--- smart-contract/hardhat.config.js
+++ smart-contract/hardhat.config.js
@@ -1,6 +1,26 @@
-require("@nomicfoundation/hardhat-toolbox");
+// require("@nomicfoundation/hardhat-toolbox");
 
-/** @type import('hardhat/config').HardhatUserConfig */
+/** @type import('hardhat/config').HardhatUserConfig 
 module.exports = {
   solidity: "0.8.19",
+}; **/
+
+require("@nomiclabs/hardhat-waffle");
+require("dotenv").config({ path: ".env" });
+require("hardhat-deploy");
+require("@nomiclabs/hardhat-ethers");
+
+module.exports = {
+  solidity: "0.7.6",
+  defaultNetwork: "telos_testnet",
+  namedAccounts: {
+    deployer: 0,
+  },
+  networks: {
+    telos_testnet: {
+      url: "https://testnet.telos.net/evm",
+      accounts: [process.env.TELOS_TESTNET_PRIVATE_KEY],
+      chainId: 41,
+    },
+  },
 };
