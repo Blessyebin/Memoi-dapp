11dbb58cac923b58a33fd90b4b6480570e53b67d
Joseph Ebin <josefophe@gmail.com>
2023-11-15 22:21:57 +0100

    Update deploy.js

--- smart-contract/scripts/deploy.js
+++ smart-contract/scripts/deploy.js
@@ -1,33 +1,24 @@
-// We require the Hardhat Runtime Environment explicitly here. This is optional
-// but useful for running the script in a standalone fashion through `node <script>`.
-//
-// You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
-// will compile your contracts, add the Hardhat Runtime Environment's members to the
-// global scope, and execute the script.
 const hre = require("hardhat");
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const unlockTime = currentTimestampInSeconds + 60;
+ const teloslearn = await hre.ethers.getContractFactory("teloslearn");
+ const telosLearn = await teloslearn.deploy();
 
-  const lockedAmount = hre.ethers.parseEther("0.001");
+ await telosLearn.deployed();
 
-  const lock = await hre.ethers.deployContract("Lock", [unlockTime], {
-    value: lockedAmount,
-  });
+ console.log("telosLearn deployed to:", telosLearn.address);
 
-  await lock.waitForDeployment();
+ let adminAddr = await telosLearn.admin();
+ console.log("admin address:", adminAddr);
 
-  console.log(
-    `Lock with ${ethers.formatEther(
-      lockedAmount
-    )}ETH and unlock timestamp ${unlockTime} deployed to ${lock.target}`
-  );
+ await telosLearn.createQuest("Introduction to Hardhat", 2, 600);
+ await telosLearn.createQuest("Unit Testing with Hardhat", 4, 500);
+ await telosLearn.createQuest("Debugging and Deploying with Hardhat", 5, 400);
 }
 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
-main().catch((error) => {
+main()
+ .then(() => process.exit(0))
+ .catch((error) => {
   console.error(error);
-  process.exitCode = 1;
-});
+  process.exit(1);
+ });
